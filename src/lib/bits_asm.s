c11c5e696e26bb805a2708f8c4f460fcbb69a4f2
Ismael <lemhadri@stanford.edu>
2020-11-16 11:35:27 +0100

    Isma's

--- src/lib/bits_asm.s
+++ src/lib/bits_asm.s
@@ -1,9 +1,6 @@
 .global count_leading_zeroes
 
+@ You implement this function
 count_leading_zeroes:
-    clz r0, r0
-    bx lr
-@    bl    pi_abort   @ included as a sanity check -- otherwise jumping here
-                     @ before it's implemented will result in running
-                     @ some random code.
-    b count_leading_zeroes
+    clz r0, r0 
+    bx lr 
